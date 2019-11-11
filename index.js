a2a1c88c747abc5a584d3d5e71007e9eaf2d6c00
Jaret Screws <jaretscrews@gmail.com>
2019-11-11 00:19:35 -0500

    Got express to send audio and images

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
 const port = 3000;
 
+app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/audio'))
+
 app.get('/', function(req, res)
 {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/html/index.html');
 });
+
+
+app.get('/test', function(req, res) {
+    var cries = fs.readdirSync('audio/1stgen');
+    let cry = "audio/1stgen/" + cries[Math.floor(Math.floor(Math.random() * cries.length))]
+    
+    console.log('sending the cry of: ' + cry);
+
+
+    fs.createReadStream(cry).pipe(res);
+})
 
 var server = app.listen(port, function() 
 {
